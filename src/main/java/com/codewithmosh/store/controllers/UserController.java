ab4b266c9f377dcdae550970f5da8918891a364f
brendanobilo <brendanxure@outlook.com>
2025-07-14 19:39:42 -0400

    Update

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -2,16 +2,16 @@ package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,10 +19,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getUsers() {
-        return userRepository.findAll().stream().map(user -> new UserDto(user.getId(), user.getName(), user.getEmail())).toList();
+    public Iterable<UserDto> getUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sort) {
+        if(!Set.of("name", "email").contains(sort)) sort = "name";
+        return userRepository.findAll(Sort.by(sort)).stream().map(userMapper::toDto).toList();
     };
 
     @GetMapping("/{id}")
@@ -31,7 +33,7 @@ public class UserController {
         if(user == null){
             return ResponseEntity.notFound().build();
         }
-        var response = new UserDto(user.getId(), user.getName(), user.getEmail());
-        return ResponseEntity.ok(response);
+
+        return ResponseEntity.ok(userMapper.toDto(user));
     }
 }
--- src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -1,7 +1,18 @@
 package com.codewithmosh.store.repositories;
 
+import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategoryId(Byte categoryId);
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.category")
+    List<Product> findAllProductsWIthCategory();
 }
